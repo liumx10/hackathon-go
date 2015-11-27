10fd76e7e6046240f1fc8090844fb34712a603f8
liumx10 <liumengxing2010@qq.com>
2015-11-27 17:03:42 +0800

    fix foods bug

--- src/api/foods.go
+++ src/api/foods.go
@@ -15,19 +15,26 @@ type Food struct {
 	Stock int `json:"stock"`
 }
 type Foods struct {
-	Foods []Food
-	m     *sync.RWMutex
+	Foods   map[int]Food
+	FoodsId []int
+	count   int
+	m       *sync.RWMutex
 }
 
 func (foods *Foods) update(id int, st int) {
-	foods.Foods[id].Stock = st
+	food := foods.Foods[id]
+	foods.Foods[id] = Food{id, food.Price, st}
 }
 func (foods *Foods) add(id int, price int, stock int) {
-	foods.Foods[id] = Food{id, price, stock} //, new(sync.RWMutex)}
+	foods.FoodsId = append(foods.FoodsId, id)
+	foods.Foods[id] = Food{id, price, stock}
+	foods.count++
 }
 func (foods *Foods) init() {
-	foods.Foods = make([]Food, 128)
 	foods.m = new(sync.RWMutex)
+	foods.FoodsId = make([]int, 0, 100)
+	foods.Foods = make(map[int]Food)
+	foods.count = 0
 }
 func (foods *Foods) check(id int) bool {
 	return id < 101 && id > 0
@@ -79,18 +86,16 @@ func InitFood() {
 		defer ReturnClient(client)
 		for {
 			foods.m.Lock()
-			for i := 1; i < 101; i++ {
+			for i := range foods.FoodsId {
 				stock, _ := client.Get("food:" + strconv.Itoa(i) + ":stock").Result()
-				//		foods.Foods[i].m.Lock()
-				foods.Foods[i].Stock, _ = strconv.Atoi(stock)
-				//		foods.Foods[i].m.Unlock()
+				st, _ := strconv.Atoi(stock)
+				foods.update(i, st)
 			}
 			foods.m.Unlock()
 			time.Sleep(1000 * time.Millisecond)
-			fmt.Println("tick")
 		}
 	}()
-	
+
 }
 
 func FoodsHandler(w http.ResponseWriter, r *http.Request) {
@@ -107,6 +112,10 @@ func FoodsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	//fmt.Println("foods: 42 ", foods.Foods[42].Stock)
 	foods.m.RLock()
-	Response(w, 200, foods.Foods[1:101])
+	list := make([]Food, 0, 100)
+	for i := range foods.FoodsId {
+		list = append(list, foods.Foods[foods.FoodsId[i]])
+	}
+	Response(w, 200, list[0:foods.count])
 	foods.m.RUnlock()
 }
